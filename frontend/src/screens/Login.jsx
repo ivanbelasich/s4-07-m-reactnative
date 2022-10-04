f515e1a2f561f7a91768fb0f40317aa3f545d3e2
juan <juan.molina159@gmail.com>
2022-10-04 17:02:35 -0300

    nueva screen login

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -15,44 +15,61 @@ export default function Notification() {
 
             <Image source={require("../assets/Login/Logo.png")} className="w-[222px] h-[82px]" />
 
-            <View className="my-10">
+            <View className="my-8">
                 <Text className="text-center">Ingresa</Text>
                 <Text className="text-center">para continuar</Text>
             </View>
-            <TextInput
-                onChangeText={onChangeUser}
-                value={user}
-                placeholder="Usuario"
-                className="py-1 px-3  bg-[#D9D9D9] rounded-full w-[307px] h-[39px] mb-10"
-            />
-            <TextInput
-                secureTextEntry={true}
-                onChangeText={onChangePassword}
-                value={password}
-                placeholder="Contraseña"
-                keyboardType="password"
-                className="py-1 px-3  bg-[#D9D9D9] rounded-full w-[307px] h-[39px] mb-10"
-            />
-            <TouchableOpacity className="w-1/2 py-2 border bg-dark-purple rounded-full mb-5">
+            <View>
+                <Text className="ml-4 font-bold">Mail</Text>
+                <TextInput
+                    onChangeText={onChangeUser}
+                    value={user}
+                    placeholder="Usuario"
+                    className="py-1 px-3  bg-[#D9D9D9] rounded-xl w-[307px] h-[39px] mb-10"
+                />
+            </View>
+            <View>
+                <Text className="ml-4 font-bold">Contraseña</Text>
+                <TextInput
+                    secureTextEntry={true}
+                    onChangeText={onChangePassword}
+                    value={password}
+                    placeholder="Contraseña"
+                    keyboardType="password"
+                    className="py-1 px-3  bg-[#D9D9D9] rounded-xl w-[307px] h-[39px] mb-10"
+                />
+            </View>
+            <View className="flex-row items-center my-10">
+                <Text className="w-[104px]">Olvidaste tu contraseña?</Text>
+                <TouchableOpacity className="">
+                    <Text className="underline color-dark-purple">toca aqui para recuperarla</Text>
+                </TouchableOpacity>
+            </View>
+
+
+            <TouchableOpacity className="w-1/2 py-2 border bg-dark-purple rounded-xl mb-20">
                 <Text className="text-center text-white">INGRESAR</Text>
             </TouchableOpacity>
-            <TouchableOpacity className="mb-20">
-                <Text>Perdio Contraseña?</Text>
-            </TouchableOpacity>
 
 
-            <View className="flex-row mb-20 justify-between w-full">
-            <TouchableOpacity className="w-[150px] h-[36px] border border-dark-purple rounded-full flex-row items-center justify-center">
-                    <Image source={require("../assets/Login/google.png")} className="h-[20px] w-[20px] mr-[10px]"/>
+
+            <View className="flex-row mb-10 justify-between w-full">
+                <TouchableOpacity className="w-[150px] h-[36px] border border-dark-purple rounded-xl flex-row items-center justify-center">
+                    <Image source={require("../assets/Login/google.png")} className="h-[20px] w-[20px] mr-[10px]" />
                     <Text className="text-center text-dark-purple">Google</Text>
                 </TouchableOpacity>
-                <TouchableOpacity className="w-[150px] h-[36px] border border-dark-purple rounded-full flex-row items-center justify-center">
-                    <Image source={require("../assets/Login/facebook.png")} className="h-[20px] w-[20px] mr-[10px]"/>
+                <TouchableOpacity className="w-[150px] h-[36px] border border-dark-purple rounded-xl flex-row items-center justify-center">
+                    <Image source={require("../assets/Login/facebook.png")} className="h-[20px] w-[20px] mr-[10px]" />
                     <Text className="text-center text-dark-purple">Facebook</Text>
                 </TouchableOpacity>
             </View>
 
-            <Text>No tienes cuenta? Registrate</Text>
+            <View className="flex-row items-center">
+                <Text className="w-[153px]">Eres nuevo en TA$KS?</Text>
+                <TouchableOpacity className="">
+                    <Text className="underline color-dark-purple">crear cuenta</Text>
+                </TouchableOpacity>
+            </View>
 
         </View>
     )
