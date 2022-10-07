ac7a3dce441e1b764923e83b8ec29b7f15dd790e
astradae <astradaezequiel.tpi@gmail.com>
2022-10-07 16:35:43 -0300

    add login register

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { RegisterAuthDto } from './dto/register-auth.dto';
+
+@ApiTags('auth')
+@Controller('/api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  registerUser(@Body() userObject: RegisterAuthDto) {
+    return this.authService.register(userObject);
+  }
+
+  @Post('login')
+  loginUser(@Body() userObject: LoginAuthDto) {
+    return this.authService.login1(userObject);
+  }
+}
