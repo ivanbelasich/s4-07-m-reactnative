ace1bc8b660062a80aa2c1b2753976ebc81f336a
astradae <astradaezequiel.tpi@gmail.com>
2022-10-10 00:57:50 -0300

    add new prop

--- backend/src/users/schema/user.schema.ts
+++ backend/src/users/schema/user.schema.ts
@@ -5,13 +5,28 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-  @Prop()
-  name: string;
+  @Prop({ required: true })
+  nombreCompleto: string;
 
   @Prop({ unique: true })
   email: string;
 
-  @Prop()
+  @Prop({ default: 'Sin definir' })
+  genero: string;
+
+  @Prop({ default: Date.now() })
+  fechaDeNacimiento: Date;
+
+  @Prop({ default: 'Sin definir' })
+  pais: string;
+
+  @Prop({ default: 'Sin definir' })
+  provincia: string;
+
+  @Prop({ default: 'Sin definir' })
+  ciudad: string;
+
+  @Prop({ required: true })
   password: string;
 }
 
