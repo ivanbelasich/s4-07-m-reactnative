32278fb877594e23b8e603ef1e42c1b9049f3103
astradae <astradaezequiel.tpi@gmail.com>
2022-10-12 19:47:00 -0300

    refactor jobcards

--- backend/src/jobcards/jobcards.service.ts
+++ backend/src/jobcards/jobcards.service.ts
@@ -3,21 +3,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateJobcardDto } from './dto/create-jobcard.dto';
 import { UpdateJobcardDto } from './dto/update-jobcard.dto';
-import { JobCards, JobCardsDocument } from './schema/jobcards.schema';
+import { JobCard, JobCardsDocument } from './schema/jobcards.schema';
 
 @Injectable()
 export class JobcardsService {
   constructor(
-    @InjectModel(JobCards.name) private jobCardsModule: Model<JobCardsDocument>,
+    @InjectModel(JobCard.name) private jobCardModule: Model<JobCardsDocument>,
   ) {}
 
   async create(createJobcardDto: CreateJobcardDto) {
-    const jobcardCreated = await this.jobCardsModule.create(createJobcardDto);
+    const jobcardCreated = await this.jobCardModule.create(createJobcardDto);
     return jobcardCreated;
   }
 
   async findAll() {
-    const list = await this.jobCardsModule.find({});
+    const list = await this.jobCardModule.find({});
     return list;
   }
 
