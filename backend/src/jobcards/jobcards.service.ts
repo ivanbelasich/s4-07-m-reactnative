461687ffe4d90f2566fd3031218ebc6b0d9d45ed
astradae <astradaezequiel.tpi@gmail.com>
2022-10-15 01:18:01 -0300

    fr jobcards.service

--- backend/src/jobcards/jobcards.service.ts
+++ backend/src/jobcards/jobcards.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { CreateJobcardDto } from './dto/create-jobcard.dto';
 import { UpdateJobcardDto } from './dto/update-jobcard.dto';
 import { JobCard, JobCardsDocument } from './schema/jobcards.schema';
@@ -8,29 +8,49 @@ import { JobCard, JobCardsDocument } from './schema/jobcards.schema';
 @Injectable()
 export class JobcardsService {
   constructor(
-    @InjectModel(JobCard.name) private jobCardModule: Model<JobCardsDocument>,
+    @InjectModel(JobCard.name) private jobCardModel: Model<JobCardsDocument>,
   ) {}
 
   async create(createJobcardDto: CreateJobcardDto) {
-    const jobcardCreated = await this.jobCardModule.create(createJobcardDto);
+    const jobcardCreated = await this.jobCardModel.create(createJobcardDto);
     return jobcardCreated;
   }
 
   async findAll() {
-    const list = await this.jobCardModule.find({});
+    const list = await this.jobCardModel.find({});
     return list;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} jobcard`;
+  async findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new HttpException('INVALID_ID', 404);
+    const jobcard = await this.jobCardModel.findById(id);
+    if (!jobcard) throw new HttpException('JOBCARD_NOT_FOUND', 404);
+
+    return jobcard;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  update(id: number, updateJobcardDto: UpdateJobcardDto) {
-    return `This action updates a #${id} jobcard`;
+  async update(id: string, updateJobcardDto: UpdateJobcardDto) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new HttpException('INVALID_ID', 404);
+    const jobcard = await this.jobCardModel.findById(id);
+    if (!jobcard) throw new HttpException('JOBCARD_NOT_FOUND', 404);
+    const updatedJobcard = await this.jobCardModel.findByIdAndUpdate(
+      id,
+      updateJobcardDto,
+      { new: true },
+    );
+    return updatedJobcard;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} jobcard`;
+  async remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new HttpException('INVALID_ID', 404);
+    const jobcard = await this.jobCardModel.findById(id);
+    if (!jobcard) throw new HttpException('JOBCARD_NOT_FOUND', 404);
+    const deletedJobcard = await this.jobCardModel.findOneAndDelete({
+      _id: id,
+    });
+    return deletedJobcard;
   }
 }
