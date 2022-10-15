050808dbfc28a64f8449463c07b82e3e0e5e980d
astradae <astradaezequiel.tpi@gmail.com>
2022-10-14 23:59:21 -0300

    add findjobcards

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,6 +2,7 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { JobCard, JobCardsDocument } from 'src/jobcards/schema/jobcards.schema';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './schema/user.schema';
@@ -10,10 +11,9 @@ import { User, UserDocument } from './schema/user.schema';
 export class UsersService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    @InjectModel(JobCard.name)
+    private readonly jobcardModel: Model<JobCardsDocument>,
   ) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
 
   async findAll() {
     return await this.userModel.find({}).populate('jobcards');
@@ -42,5 +42,12 @@ export class UsersService {
     if (!user) throw new HttpException('USER_NOT_FOUND', 404);
     const deletedUser = await this.userModel.findOneAndDelete({ _id: id });
     return deletedUser;
+  }
+
+  async findJobcards(id: string) {
+    const user = await this.userModel.findById(id);
+    if (!user) throw new HttpException('USER_NOT_FOUND', 404);
+    const jobcards = await this.jobcardModel.find({ userId: id });
+    return jobcards;
   }
 }
