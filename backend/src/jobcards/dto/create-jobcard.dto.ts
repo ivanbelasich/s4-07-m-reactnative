f8ba4b782bbbbc8058e9d193b8eee0b44c469cfe
astradae <astradaezequiel.tpi@gmail.com>
2022-10-14 23:16:56 -0300

    add apiProperty

--- backend/src/jobcards/dto/create-jobcard.dto.ts
+++ backend/src/jobcards/dto/create-jobcard.dto.ts
@@ -1,14 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, Min } from 'class-validator';
 
 export class CreateJobcardDto {
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Nombre de jobcard',
+    example: 'Cortar el pasto',
+  })
   name: string;
 
   @IsNotEmpty()
   @IsNumber()
   @Min(1)
+  @ApiProperty({
+    description: 'Precio de jobcard',
+    example: 1500,
+  })
   price: number;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Descripcion de jobcard',
+    example: 'Cortar el pasto de mi patio 4x5 mts',
+  })
   descripcion: string;
+
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'UserId del usuario que crea la jobcard ',
+    example: '634742c61f645b0765e7a2c3',
+  })
+  userId: string;
 }
