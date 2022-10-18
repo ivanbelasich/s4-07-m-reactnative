446e403181b34030797cffc31a9e9671f22bbae4
Ivan <ivanbelasich@gmail.com>
2022-10-18 17:03:07 -0300

    store modified

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,15 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import jobsReducer from "../features/jobs/JobsSlice";
-import { userApi } from "./apis/userApi";
 import userReducer from "../features/user/UserSlice";
 
 const store = configureStore({
   reducer: {
-    jobs: jobsReducer,
     user: userReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
 });
 
 export default store;
