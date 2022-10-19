2a8b7e968165f518aa92f702a74938d91d1376e7
Ivan <ivanbelasich@gmail.com>
2022-10-19 20:20:18 -0300

    shadow-buttons

--- frontend/src/components/Profile/VioletButton.js
+++ frontend/src/components/Profile/VioletButton.js
@@ -1,19 +1,18 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function VioletButton(props) {
   return (
-    <TouchableOpacity className="">
+    <TouchableOpacity className="shadow-xl  shadow-[#570E7E] rounded-xl">
       <LinearGradient
         colors={["#570E7E", "#AA7BC3"]}
         className="h-10 rounded-[10px] "
-     
       >
         <View>
           <Text
             onPress={props.onPress}
-            className="text-[#f1f1f1] text-xs text-center py-3  m-auto font-extrabold w-[135px] "
+            className="text-[#f1f1f1] text-xs text-center py-3  m-auto font-extrabold w-[135px]  "
           >
             {props.title}
           </Text>
--- frontend/src/components/Profile/WhiteButton.js
+++ frontend/src/components/Profile/WhiteButton.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const WhiteButton = (props) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className="shadow-xl  shadow-[#570E7E] rounded-xl">
       <Text className="border-2 h-10 p-3 font-extrabold text-xs border-[#5D1683] rounded-xl bg-white color-[#531CB3] text-center w-[135px]">
         {props.title}
       </Text>
