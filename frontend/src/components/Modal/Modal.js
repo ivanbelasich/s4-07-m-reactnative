04dd1421b309aeb5a03380759ae8d1e27198bcca
Ivan <ivanbelasich@gmail.com>
2022-10-21 12:01:03 -0300

    modal listo

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import { View, Text, Modal, Pressable, Alert } from "react-native";
-import React,{ useState}from "react";
+import React, { useState } from "react";
 import VioletButton from "../Profile/VioletButton";
 import { useNavigation } from "@react-navigation/native";
 
@@ -8,34 +8,31 @@ export default function ModalContainer(props) {
 
   return (
     <View>
-      <Modal animationType="slide"
+      <Modal
+        animationType="slide"
         transparent={true}
         visible={showModal}
         onRequestClose={() => {
           Alert.alert("Modal has been closed.");
           setShowModal(!showModal);
-        }}>
+        }}
+      >
         <View className="bg-[#D2A8E8] w-[305px] m-auto p-10 shadow-2xl shadow-[#724BB6]">
           <Text className="text-lg font-extrabold color-[#570E7E] text-center leading-[22px]">
             {props.title}
           </Text>
-      
+
           <Text className="text-[15px] font-semibold text-center color-[#FCFCFC] mt-4 leading-[18px] mb-6]">
             {props.message}
           </Text>
-         {/*  <Pressable onPress={() =>   setShowModal(!showModal)}>
-          <Text>  XXXX         </Text>
-      
-        </Pressable> */}
-        <VioletButton title='xx' onPress={() =>   setShowModal(!showModal)}/> 
+          <VioletButton
+            title="Aceptar"
+            onPress={() => setShowModal(!showModal)}
+          />
         </View>
       </Modal>
-
-      <Pressable
-    
-        onPress={() =>   setShowModal(true)}
-      >
-        <Text >Show Modal</Text>
+      <Pressable onPress={() => setShowModal(true)}>
+        <Text>Show Modal</Text>
       </Pressable>
     </View>
   );
