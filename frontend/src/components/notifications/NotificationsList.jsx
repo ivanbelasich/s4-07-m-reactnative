194b39cfdacaf721e41813bd88581c4e9d66fbcf
Ivan <ivanbelasich@gmail.com>
2022-10-25 20:47:37 -0300

    notif css fix

--- frontend/src/components/notifications/NotificationsList.jsx
+++ frontend/src/components/notifications/NotificationsList.jsx
@@ -4,24 +4,23 @@ import Notification from "./Notification";
 import { useNavigation } from "@react-navigation/native";
 
 const NotificationsList = ({ data, userData }) => {
-    console.log({data}+"notificaciones")
-    const navigation = useNavigation();
+  console.log({ data } + "notificaciones");
+  const navigation = useNavigation();
 
-    const render = ({ item }) => (
-        <Notification
-            item={item}
-            // onPress={() => navigation.navigate("Detalle", { value: item, data: userData })}
-        />
-    );
+  const render = ({ item }) => (
+    <Notification
+      item={item}
+      // onPress={() => navigation.navigate("Detalle", { value: item, data: userData })}
+    />
+  );
 
-    return (
-        <FlatList
-            data={data}
-            renderItem={render}
-             keyExtractor={(item) => item._id}
-            className="mb-auto"
-        />
-    );
+  return (
+    <FlatList
+      data={data}
+      renderItem={render}
+      keyExtractor={(item) => item._id}
+    />
+  );
 };
 
 export default NotificationsList;
